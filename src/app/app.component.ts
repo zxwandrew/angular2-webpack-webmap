35d416dad96064899d24e640f8ad9d399cae7053
Andrew Wang <zxwandrew@gmail.com>
2016-08-19 23:18:27 -0700

    works with esri 3.x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MapComponent } from './map.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  directives: [MapComponent],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
+
 })
 export class AppComponent {
-  title = 'app works!';
+
+  // references to child components
+  @ViewChild(MapComponent) mapComponent:MapComponent;
+
+  title = 'Map Title';
+
+  // map config
+  itemId = '9fbc6b9438e74d869b8ab9d3d4395ace';
+  public mapOptions = {
+    basemap: 'gray',
+    center: [-97, 38], // lon, lat
+    zoom: 5
+  };
+
+  onMapLoad(response) {
+    const map = response.map;
+  }
+
 }
